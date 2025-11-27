176c8732b9248ef5f5e18b0f39f4cfdb0c8fded1
sarisa-28 <115571611+sarisa-28@users.noreply.github.com>
2025-11-27 15:01:24 +0700

    first commit

--- server.js
+++ server.js
@@ -17,18 +17,11 @@ console.log("MONGO_URI from .env =", process.env.MONGO_URI);
 if (!fs.existsSync(UPLOAD_DIR)) {
   fs.mkdirSync(UPLOAD_DIR, { recursive: true });
 }
-const corsOptions = {
-    origin: 'https://register-exam.onrender.com',
-    methods: ['GET', 'POST'],
-    allowedHeaders: ['Content-Type'],
-};
 
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
-
-
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
@@ -154,7 +147,7 @@ app.post('/api/register',
         email: req.body.email,
         examCenter: req.body.examCenter,
         subjects,
-        photoPath: `https://register-exam.onrender.com/uploads/${path.basename(req.file.path)}`
+        photoPath: `http://localhost:3000/uploads/${path.basename(req.file.path)}`
       });
 
       await doc.save();
